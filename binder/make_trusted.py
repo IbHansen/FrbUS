5024f736126db891fc6b5a24d09e8a621eebbcda
IbHansen <43575358+IbHansen@users.noreply.github.com>
2021-10-29 13:02:09 +0200

    better

--- binder/make_trusted.py
+++ binder/make_trusted.py
@@ -12,10 +12,15 @@ with open('Postbuild','wt') as f:
 #!/bin/bash 
 ''')
     for dir in Path('../').glob('**'):
-        if len(dir.parts) and str(dir.parts[-1]).startswith('.'): continue
+        # print(dir.parts)
+
+        if len(dir.parts)>1 and str(dir.parts[1]).startswith('.'): continue
+        print(dir)
         for notebook in dir.glob('*.ipynb'):
             n = PurePosixPath(notebook)
             f.write(f'jupyter trust "{str(n).split("/",1)[1]}"\n')
     f.write('jupyter nbextension enable hide_input_all/main\n')  
-    f.write('jupyter nbextension enable splitcell/splitcell\n')     
+    f.write('jupyter nbextension enable splitcell/splitcell\n') 
+    f.write('python frbus-run-numba.py\n') 
     f.write('exec "$@"')
+    # breakpoint()
